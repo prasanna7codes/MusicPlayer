3324700e9814bc18b21c176c119752a08aafbeda
prasanna7codes <prasannalit098@gmail.com>
2025-03-21 19:34:39 +0530

    adding play pause skip function

---
+++ MuiscPlayerServices.js
@@ -0,0 +1,29 @@
+import TrackPlayer from "react-native-track-player";
+
+import { playListData } from "./src/constants";
+
+export async function playbackService () {
+
+    TrackPlayer.addEventListener(Event.RemotePlay, () => {
+        TrackPlayer.play()
+
+    });
+
+    TrackPlayer.addEventListener(Event.RemotePause, () => {
+        TrackPlayer.pause()
+
+    });
+
+
+    TrackPlayer.addEventListener(Event.RemoteNext, () => {
+        TrackPlayer.skipToNext()
+
+    });
+
+
+    TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+        TrackPlayer.skipToPrevious()
+
+    });
+
+}
